15428f55b715dd2bb997fbeb16a90f642bee69b9
8bitSaiyan <prithvi202.codes@gmail.com>
2025-08-28 12:45:51 +0530

    Added function prototypes
    Signed-off-by: 8bitSaiyan <prithvi202.codes@gmail.com>

--- movie_rating_system.c
+++ movie_rating_system.c
@@ -11,3 +11,17 @@ typedef struct {
     int releasedYear;
     char watchedDate[10];
 } Movie;
+
+// Global movie collection
+Movie movies[MAX_MOVIES];
+int movieCount = 0;
+
+// Function Prototypes
+void showMenu();
+void addMovie();
+void viewAllMovies();
+void searchMovie();
+void sortMoviesByRating();
+void saveMoviesToFile();
+void loadMoviesFromFile();
+void getCurrentDate(char *buffer, size_t size);
