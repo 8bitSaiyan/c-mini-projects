d997a46e35afc3d36fb5ca6909f0c815319d00dd
8bitSaiyan <prithvi202.codes@gmail.com>
2025-08-28 12:49:56 +0530

    Added function placeholders
    Signed-off-by: 8bitSaiyan <prithvi202.codes@gmail.com>

--- movie_rating_system.c
+++ movie_rating_system.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_MOVIES 100
 
@@ -25,3 +26,38 @@ void sortMoviesByRating();
 void saveMoviesToFile();
 void loadMoviesFromFile();
 void getCurrentDate(char *buffer, size_t size);
+
+
+void showMenu() {
+    printf("\n=== Personal Movie Rating System ===\n");
+    printf("1. Add Movie\n");
+    printf("2. View All Movies\n");
+    printf("3. Search Movie\n");
+    printf("4. Sort by Rating\n");
+    printf("5. Save & Exit\n");
+}
+
+void addMovie() {
+    printf("Logic not written yet.\n");
+}
+
+void viewAllMovies() {
+    printf("Logic not written yet.\n");
+}
+
+void searchMovie() {
+    printf("Logic not written yet.\n");
+}
+
+void sortMoviesByRating() {
+    printf("Logic not written yet.\n");
+}
+
+void saveMoviesToFile() {
+    printf("Logic not written yet.\n");
+}
+
+void loadMoviesFromFile() {
+    printf("Logic not written yet.\n");
+}
+
